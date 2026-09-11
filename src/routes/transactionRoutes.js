0db2563b9138f372576625a1b9a26d2d72335abb
3DEtechnology <it@3dtechproto.com>
2026-09-11 10:10:41 +0530

    Refactor transactionRoutes.js for consistency

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,15 +1,16 @@
-const router =
-require("express").Router();
+const router = require("express").Router();
 
 const transactionController =
-require(
-    "../controllers/transactionController"
-);
+    require("../controllers/transactionController");
+
+const authMiddleware =
+    require("../middleware/authMiddleware");
+
+router.use(authMiddleware);
 
 router.get(
     "/",
     transactionController.getTransactions
 );
 
-module.exports =
-router;
+module.exports = router;
